27ec9525c85457d1212574eb50849e3aee1dbd18
Nikhil kurmi <89118026+nikhilnew@users.noreply.github.com>
2023-08-08 19:37:31 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2');
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'sys'
+  host: "103.228.83.115", 
+  user: "root", // USER NAME
+  database: "sendquickmail_db", // DATABASE NAME
+  password: "Cylsys@678",
 });
 dbConn.connect(function(err) {
   if (err) throw err;
